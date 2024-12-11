c5f2f0969f22dba8a535c2c0e1d14e5ae22119ee
Trent Nix <trentonnix@hotmail.com>
2024-12-11 12:04:23 -0600

    cleaned up Day 11 tests

--- exercise/day11_test.go
+++ exercise/day11_test.go
@@ -4,19 +4,7 @@ import (
 	"testing"
 )
 
-func compareStringSlices(a, b []string) bool {
-	if len(a) != len(b) {
-		return false
-	}
-	for i := range a {
-		if a[i] != b[i] {
-			return false
-		}
-	}
-	return true
-}
-
-func TestDay11Part1(t *testing.T) {
+func TestDay11(t *testing.T) {
 	input := "125 17"
 
 	d11 := Day11{}
@@ -38,7 +26,4 @@ func TestDay11Part1(t *testing.T) {
 	if calculatedValue != expectedValue {
 		t.Errorf("Day 11 - Part 1 Test:\nwant %v\ngot %v\n", expectedValue, calculatedValue)
 	}
-}
-
-func TestDay11Part2(t *testing.T) {
 }
