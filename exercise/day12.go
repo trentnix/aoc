77dd28f6ac15c4d4a27ca7f1ffb125f31f1bf5f8
Trent Nix <trentonnix@hotmail.com>
2024-12-12 09:20:18 -0600

    Day 12 - Part 1 done

--- exercise/day12.go
+++ exercise/day12.go
@@ -14,6 +14,18 @@ type (
 		name string
 		file string
 	}
+
+	Garden           [][]rune
+	GardenCoordinate struct {
+		x, y int
+	}
+	GardenSections       map[GardenCoordinate][]GardenCoordinate
+	GardenSectionDetails struct {
+		plant     rune
+		id        GardenCoordinate
+		area      int
+		perimeter int
+	}
 )
 
 // GetName returns the name of the Day 12 exercise
@@ -39,16 +51,39 @@ func (d *Day12) Run(w io.Writer) {
 
 // RunFromInput executs the Day 12 solution using the provided input data
 func (d *Day12) RunFromInput(w io.Writer, input []string) {
-	// data, err := // parse the data
-	// if err != nil {
-	// 	w.Write([]byte(fmt.Sprintf("There was an error trying to parse the input: %v.", err)))
-	// 	return
-	// }
+	garden := d.parseInput(input)
+
+	// part 1
+	totalPrice := d.Part1(garden)
+	w.Write([]byte(fmt.Sprintf("Day 12 - Part 1 - The total price of fencing all regions is %d.\n", totalPrice)))
 }
 
-// Part1
-func (d *Day12) Part1() int {
-	return 0
+// Part1 calculates the price of the fence by finding each Garden section, calculating
+// the area, calculating the perimeter, multiplying the area * perimeter, and adding the
+// product. The sum of the products for each area is the total price of the fencing,
+// per the assignment.
+func (d *Day12) Part1(garden Garden) int {
+	sections := d.extractSections(garden)
+	var sectionDetails []GardenSectionDetails
+
+	for coordinate, section := range sections {
+		details := GardenSectionDetails{
+			id:    coordinate,
+			plant: garden[coordinate.y][coordinate.x],
+			area:  len(section),
+		}
+
+		details.perimeter = d.calculatePerimeter(garden, section)
+
+		sectionDetails = append(sectionDetails, details)
+	}
+
+	price := 0
+	for _, details := range sectionDetails {
+		price += details.area * details.perimeter
+	}
+
+	return price
 }
 
 // Part2
@@ -57,5 +92,103 @@ func (d *Day12) Part2() int {
 }
 
 // parseInput
-func (d *Day12) parseInput(input []string) /* return types */ {
+func (d *Day12) parseInput(input []string) Garden {
+	var garden Garden
+	for _, row := range input {
+		garden = append(garden, []rune(row))
+	}
+	return garden
+}
+
+// Helper function to check if a coordinate is within bounds
+func (d *Day12) isInBounds(garden Garden, x, y int) bool {
+	return x >= 0 && x < len(garden) && y >= 0 && y < len(garden[0])
+}
+
+// findSection finds sections using flood-fill
+func (d *Day12) findSection(garden Garden, visited map[GardenCoordinate]bool, x, y int) []GardenCoordinate {
+	runeValue := garden[x][y]
+	section := []GardenCoordinate{}
+	stack := []GardenCoordinate{{x, y}}
+
+	// Perform DFS to collect all connected coordinates
+	for len(stack) > 0 {
+		coord := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		// skip already visited coordinates
+		if visited[coord] {
+			continue
+		}
+
+		visited[coord] = true
+		section = append(section, coord)
+
+		// check neighbors
+		directions := []GardenCoordinate{
+			{0, 1},  // right
+			{1, 0},  // down
+			{0, -1}, // left
+			{-1, 0}, // up
+		}
+
+		for _, dir := range directions {
+			nx, ny := coord.x+dir.x, coord.y+dir.y
+			if d.isInBounds(garden, nx, ny) && !visited[GardenCoordinate{nx, ny}] && garden[nx][ny] == runeValue {
+				stack = append(stack, GardenCoordinate{nx, ny})
+			}
+		}
+	}
+
+	return section
+}
+
+// extractSections creates an instance of GardenSections from the specified Garden
+func (d *Day12) extractSections(garden Garden) GardenSections {
+	sections := make(GardenSections)
+	visited := make(map[GardenCoordinate]bool)
+
+	for x := 0; x < len(garden); x++ {
+		for y := 0; y < len(garden[0]); y++ {
+			coord := GardenCoordinate{x, y}
+
+			// If not visited, it's a new section
+			if !visited[coord] {
+				section := d.findSection(garden, visited, x, y)
+				sections[coord] = section
+			}
+		}
+	}
+
+	return sections
+}
+
+// calculatePerimeter calculates the perimeter of a given Garden section using
+func (d *Day12) calculatePerimeter(garden Garden, section []GardenCoordinate) int {
+	// set used to check if a coordinate belongs to the section
+	sectionSet := make(map[GardenCoordinate]bool)
+	for _, coord := range section {
+		sectionSet[coord] = true
+	}
+
+	perimeter := 0
+
+	directions := []GardenCoordinate{
+		{0, 1},  // right
+		{1, 0},  // down
+		{0, -1}, // left
+		{-1, 0}, // up
+	}
+
+	for _, coord := range section {
+		for _, dir := range directions {
+			neighbor := GardenCoordinate{coord.x + dir.x, coord.y + dir.y}
+			if !d.isInBounds(garden, neighbor.x, neighbor.y) || !sectionSet[neighbor] {
+				// neighbor is not out of bounds and is part of the section
+				perimeter++
+			}
+		}
+	}
+
+	return perimeter
 }
--- exercise/day12_test.go
+++ exercise/day12_test.go
@@ -5,27 +5,71 @@ import (
 )
 
 func TestDay12Part1Simple(t *testing.T) {
-	// input := []string{
-	// 	"9990999",
-	// 	"9991999",
-	// 	"9992999",
-	// 	"6543456",
-	// 	"7111117",
-	// 	"8111118",
-	// 	"9111119",
-	// }
+	input := []string{
+		"AAAA",
+		"BBCD",
+		"BBCC",
+		"EEEC",
+	}
 
-	// d12 := Day12{}
+	d12 := Day12{}
 
-	// topo := d12.parseInput(input)
+	garden := d12.parseInput(input)
 
-	// calculatedValue := d12.Part1(topo)
-	// expectedValue := 2
+	calculatedValue := d12.Part1(garden)
+	expectedValue := 140
 
-	// if calculatedValue != expectedValue {
-	// 	t.Errorf("Day 12 - Part 1 Test:\nwant %v\ngot %v\n", expectedValue, calculatedValue)
-	// }
+	if calculatedValue != expectedValue {
+		t.Errorf("Day 12 - Part 1 Test:\nwant %v\ngot %v\n", expectedValue, calculatedValue)
+	}
 }
 
-func TestDay12Part1Simple2(t *testing.T) {
+func TestDay12Part1Simple1(t *testing.T) {
+	input := []string{
+		"OOOOO",
+		"OXOXO",
+		"OOOOO",
+		"OXOXO",
+		"OOOOO",
+	}
+
+	d12 := Day12{}
+
+	garden := d12.parseInput(input)
+
+	calculatedValue := d12.Part1(garden)
+	expectedValue := 772
+
+	if calculatedValue != expectedValue {
+		t.Errorf("Day 12 - Part 1 Test:\nwant %v\ngot %v\n", expectedValue, calculatedValue)
+	}
+}
+
+func TestDay12Part1(t *testing.T) {
+	input := []string{
+		"RRRRIICCFF",
+		"RRRRIICCCF",
+		"VVRRRCCFFF",
+		"VVRCCCJFFF",
+		"VVVVCJJCFE",
+		"VVIVCCJJEE",
+		"VVIIICJJEE",
+		"MIIIIIJJEE",
+		"MIIISIJEEE",
+		"MMMISSJEEE",
+	}
+
+	d12 := Day12{}
+
+	garden := d12.parseInput(input)
+
+	calculatedValue := d12.Part1(garden)
+	expectedValue := 1930
+
+	if calculatedValue != expectedValue {
+		t.Errorf("Day 12 - Part 1 Test:\nwant %v\ngot %v\n", expectedValue, calculatedValue)
+	}
+}
+
+func TestDay12Part2(t *testing.T) {
 }
