c5aad15d71701d99d3c352807cee046f2a5bf9a5
Trent Nix <trentonnix@hotmail.com>
2024-12-13 12:19:19 -0600

    Day 13 - Part 2 done

--- exercise/day13.go
+++ exercise/day13.go
@@ -18,13 +18,13 @@ type (
 	ClawGame struct {
 		A              ClawGameButton
 		B              ClawGameButton
-		xPrizeLocation int
-		yPrizeLocation int
+		xPrizeLocation int64
+		yPrizeLocation int64
 	}
 
 	ClawGameButton struct {
 		price        int
-		xMove, yMove int
+		xMove, yMove int64
 	}
 )
 
@@ -51,60 +51,30 @@ func (d *Day13) Run(w io.Writer) {
 
 // RunFromInput executs the Day 13 solution using the provided input data
 func (d *Day13) RunFromInput(w io.Writer, input []string) {
-	clawGames := d.parseInput(input)
+	var numPrizesWon, sumTokensSpent int64
 
 	// part 1
-	numPrizesWon, numTokensSpent := d.Part1(clawGames)
-	w.Write([]byte(fmt.Sprintf("Day 13 - Part 1 - prizes won: %d, tokens spent: %d\n", numPrizesWon, numTokensSpent)))
+	clawGames := d.parseInput(input, false)
+	numPrizesWon, sumTokensSpent = d.Solve(clawGames)
+	w.Write([]byte(fmt.Sprintf("Day 13 - Part 1 - prizes won: %d, tokens spent: %d\n", numPrizesWon, sumTokensSpent)))
+
+	// part 2
+	clawGames = d.parseInput(input, true)
+	numPrizesWon, sumTokensSpent = d.Solve(clawGames)
+	w.Write([]byte(fmt.Sprintf("Day 13 - Part 2 - prizes won: %d, tokens spent: %d\n", numPrizesWon, sumTokensSpent)))
 }
 
-// Part1 determines which games are winnable given the input and, for each game,
-// what the minimum cost would be to win the game. The number of winnable games and
-// the sum of the minimum costs to win those games is returned
-func (d *Day13) Part1(games []ClawGame) (prizes, sumTokensSpent int) {
-	prizes = 0
-	sumTokensSpent = 0
-
+// Solve uses Cramer's rule to identify the solve for each specified ClawGame instance
+func (d *Day13) Solve(games []ClawGame) (prizes, sumTokensSpent int64) {
 	for _, game := range games {
-		x1 := game.A.xMove
-		x2 := game.B.xMove
-		x3 := game.xPrizeLocation
-
-		var solves [][2]int
-
-		abXSolves := d.solveEquation(x1, x2, x3)
-		for _, solve := range abXSolves {
-			a := solve[0]
-			b := solve[1]
-
-			yVal := a*game.A.yMove + b*game.B.yMove
-			if yVal == game.yPrizeLocation {
-				solves = append(solves, solve)
-			}
-		}
-
-		tokensSpent := 0
-		if len(solves) > 0 {
+		a, b := d.solveUsingCramersRule(game)
+		if a != 0 && b != 0 {
+			sumTokensSpent += a*int64(game.A.price) + b*int64(game.B.price)
 			prizes++
-
-			for _, solve := range solves {
-				gameCost := solve[0]*game.A.price + solve[1]*game.B.price
-				// find cheapest option
-				if tokensSpent == 0 || gameCost < tokensSpent {
-					tokensSpent = gameCost
-				}
-			}
-
-			sumTokensSpent += tokensSpent
 		}
 	}
 
 	return prizes, sumTokensSpent
-}
-
-// Part2
-func (d *Day13) Part2() int {
-	return 0
 }
 
 const (
@@ -113,22 +83,27 @@ const (
 )
 
 // parseInput parses the specified input into a slice of ClawGame instances
-func (d *Day13) parseInput(input []string) []ClawGame {
+func (d *Day13) parseInput(input []string, p2 bool) []ClawGame {
 	var games []ClawGame
 
 	// Process every 3 lines, skipping the blank line
 	for i := 0; i < len(input); i += 4 {
 		// button A
-		var aX, aY int
+		var aX, aY int64
 		fmt.Sscanf(input[i], "Button A: X+%d, Y+%d", &aX, &aY)
 
 		// button B
-		var bX, bY int
+		var bX, bY int64
 		fmt.Sscanf(input[i+1], "Button B: X+%d, Y+%d", &bX, &bY)
 
 		// prize location
-		var pX, pY int
+		var pX, pY int64
 		fmt.Sscanf(input[i+2], "Prize: X=%d, Y=%d", &pX, &pY)
+
+		if p2 {
+			pX += 10000000000000
+			pY += 10000000000000
+		}
 
 		game := ClawGame{
 			A:              ClawGameButton{xMove: aX, yMove: aY, price: priceA},
@@ -142,27 +117,42 @@ func (d *Day13) parseInput(input []string) []ClawGame {
 	return games
 }
 
-// solveEquation determines all of the a,b pairs that solve for the specified
-// input. x1 is the "A" moves, x2 is the "B" moves, and x3 is the prize location
-// for the current axis
+// solveUsingCramersRule solves for a game using Cramer's rule:
 //
-// x1 * a + x2 * b = x3 - solving for (a, b)
+// A = (p_x*b_y - prize_y*b_x) / (a_x*b_y - a_y*b_x)
+// B = (a_x*p_y - a_y*p_x) / (a_x*b_y - a_y*b_x)
 //
-// b = (x3 - x1 * a) / x2
-func (d *Day13) solveEquation(x1, x2, x3 int) [][2]int {
-	var results [][2]int
+// If integer values aren't calculated (whole numbers), then (0,0) is returned
+func (d *Day13) solveUsingCramersRule(g ClawGame) (int64, int64) {
+	pX := g.xPrizeLocation
+	pY := g.yPrizeLocation
 
-	// Iterate over possible values of 'a'
-	for a := 1; x1*a < x3; a++ {
-		// Calculate b
-		remainder := x3 - x1*a
-		if remainder > 0 && remainder%x2 == 0 {
-			b := remainder / x2
-			if b > 0 {
-				results = append(results, [2]int{a, b})
-			}
-		}
+	var denominator, numeratorA, numeratorB int64
+	denominator = g.A.xMove*g.B.yMove - g.A.yMove*g.B.xMove
+	if denominator == 0 {
+		// If the denominator is 0, the system is either inconsistent or dependent
+		return 0, 0
 	}
 
-	return results
+	numeratorA = pX*g.B.yMove - pY*g.B.xMove
+	numeratorB = g.A.xMove*pY - g.A.yMove*pX
+
+	if numeratorA%denominator != 0 {
+		// a is not an integer
+		return 0, 0
+	}
+	a := numeratorA / denominator
+
+	if numeratorB%denominator != 0 {
+		// b is not an integer
+		return 0, 0
+	}
+	b := numeratorB / denominator
+
+	if a < 0 || b < 0 {
+		return 0, 0
+	}
+
+	// (a, b) solves for the system
+	return int64(a), int64(b)
 }
--- exercise/day13_test.go
+++ exercise/day13_test.go
@@ -25,11 +25,11 @@ func TestDay13Part1(t *testing.T) {
 
 	d13 := Day13{}
 
-	games := d13.parseInput(input)
+	games := d13.parseInput(input, false)
 
-	prizesWon, minimumTokensSpent := d13.Part1(games)
-	expectedPrizesWon := 2
-	expectedTokensSpent := 480
+	prizesWon, minimumTokensSpent := d13.Solve(games)
+	expectedPrizesWon := int64(2)
+	expectedTokensSpent := int64(480)
 
 	if prizesWon != expectedPrizesWon {
 		t.Errorf("Day 13 - Part 1 (prizes) Test:\nwant %v\ngot %v\n", expectedPrizesWon, prizesWon)
@@ -40,4 +40,36 @@ func TestDay13Part1(t *testing.T) {
 }
 
 func TestDay13Part2(t *testing.T) {
+	input := []string{
+		"Button A: X+94, Y+34",
+		"Button B: X+22, Y+67",
+		"Prize: X=8400, Y=5400",
+		"",
+		"Button A: X+26, Y+66",
+		"Button B: X+67, Y+21",
+		"Prize: X=12748, Y=12176",
+		"",
+		"Button A: X+17, Y+86",
+		"Button B: X+84, Y+37",
+		"Prize: X=7870, Y=6450",
+		"",
+		"Button A: X+69, Y+23",
+		"Button B: X+27, Y+71",
+		"Prize: X=18641, Y=10279",
+	}
+
+	d13 := Day13{}
+
+	games := d13.parseInput(input, true)
+
+	prizesWon, minimumTokensSpent := d13.Solve(games)
+	expectedPrizesWon := int64(2)
+	expectedTokensSpent := int64(875318608908)
+
+	if prizesWon != expectedPrizesWon {
+		t.Errorf("Day 13 - Part 1 (prizes) Test:\nwant %v\ngot %v\n", expectedPrizesWon, prizesWon)
+	}
+	if minimumTokensSpent != expectedTokensSpent {
+		t.Errorf("Day 13 - Part 1 (tokens) Test:\nwant %v\ngot %v\n", expectedTokensSpent, minimumTokensSpent)
+	}
 }
