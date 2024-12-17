70da812dbb1418d54e0b3c23da8f553724d4d3f4
Trent Nix <trentonnix@hotmail.com>
2024-12-16 18:02:04 -0600

    Day 16 - Part 2 complete

--- exercise/day16.go
+++ exercise/day16.go
@@ -89,6 +89,11 @@ func (d *Day16) RunFromInput(w io.Writer, input []string) {
 	// part 1
 	cheapestPathCost := d.Part1(maze)
 	w.Write([]byte(fmt.Sprintf("Day 16 - Part 1 - The cheapest path through the maze costs %d.\n", cheapestPathCost)))
+
+	// part 1
+	maze = d.parseInput(input)
+	visitedNodes := d.Part2(maze)
+	w.Write([]byte(fmt.Sprintf("Day 16 - Part 2 - The number of nodes visited on the cheapest path(s) is %d.\n", visitedNodes)))
 }
 
 // Part1 traverses the maze and finds the shortest path cost from the start to
@@ -101,14 +106,35 @@ func (d *Day16) Part1(maze ReindeerMaze) int {
 	reindeerMazeGraph := buildGraph(maze)
 
 	// Find the cheapest path
-	cheapestPath := dijkstra(reindeerMazeGraph, start, end, startDirection)
+	cheapestPath := findLowestCostPath(reindeerMazeGraph, start, end, startDirection)
 
 	return cheapestPath
 }
 
-// Part2
-func (d *Day16) Part2() int {
-	return 0
+// Part2 finds the number of nodes visited along every path that happens to share
+// the lowest cost
+func (d *Day16) Part2(maze ReindeerMaze) int {
+	start := maze.findLocation('S')
+	end := maze.findLocation('E')
+	startDirection := east
+
+	reindeerMazeGraph := buildGraph(maze)
+
+	_, allPaths := findAllMinimumPaths(reindeerMazeGraph, start, end, startDirection)
+	// allPaths returns nodes, but not every position. We need to expand it to have every
+	// position in the maze, not just the graph nodes
+	expandedPaths := expandAllPaths(allPaths, reindeerMazeGraph)
+
+	visitedPositions := make(map[ReindeerMazePoint]bool)
+	for _, p := range expandedPaths {
+		for _, pos := range p {
+			// we visited this position on the specified path, set it to true
+			visitedPositions[pos] = true
+		}
+	}
+
+	// return the number of positions that were visited
+	return len(visitedPositions)
 }
 
 // parseInput converts the input into a ReindeerMaze
@@ -263,7 +289,7 @@ func isNode(maze ReindeerMaze, point ReindeerMazePoint) bool {
 	return true
 }
 
-// djikstra implement's Dijkstra's Algorithm to find the lowest cost path
+// findLowestCostPath implement's Dijkstra's Algorithm to find the lowest cost path
 // in the specified ReindeerMazeGraph. The parameters are:
 // - graph is the ReindeerMazeGraph being traversed
 // - start is the start node in the graph
@@ -271,7 +297,7 @@ func isNode(maze ReindeerMaze, point ReindeerMazePoint) bool {
 // - startDirection determines which direction from the starting point the traversal will begin
 //
 // The return value is the cost of the path that was found.
-func dijkstra(graph ReindeerMazeGraph, start, end ReindeerMazePoint, startDirection int) int {
+func findLowestCostPath(graph ReindeerMazeGraph, start, end ReindeerMazePoint, startDirection int) int {
 	pq := &PriorityQueue{}
 	heap.Init(pq)
 
@@ -332,4 +358,238 @@ func dijkstra(graph ReindeerMazeGraph, start, end ReindeerMazePoint, startDirect
 
 	// end not found
 	return -1
+}
+
+// findAllMinimumPaths implement's Dijkstra's Algorithm to find the lowest cost path and returns
+// all of the paths that have the same minimum cost in the specified ReindeerMazeGraph. The parameters are:
+// - graph is the ReindeerMazeGraph being traversed
+// - start is the start node in the graph
+// - end is the end node in the graph
+// - startDirection determines which direction from the starting point the traversal will begin
+//
+// The return value is the cost of the path that was found and
+func findAllMinimumPaths(graph ReindeerMazeGraph, start, end ReindeerMazePoint, startDirection int) (int, [][]ReindeerMazePoint) {
+	pq := &PriorityQueue{}
+	heap.Init(pq)
+
+	// visited[node][direction] = minimal cost to reach that node with that direction
+	visited := make(map[ReindeerMazePoint]map[int]int)
+
+	// parents[node][direction] = list of (node,direction) from which we arrived at this node/direction at minimal cost
+	parents := make(map[ReindeerMazePoint]map[int][]struct {
+		node      ReindeerMazePoint
+		direction int
+	})
+
+	// Initialize with the start node and direction
+	heap.Push(pq, &State{
+		node:      graph[start],
+		direction: startDirection,
+		cost:      0,
+	})
+	// We know the cost to reach start with startDirection is 0
+	if visited[start] == nil {
+		visited[start] = make(map[int]int)
+	}
+	visited[start][startDirection] = 0
+
+	// minimalEndCost: track the minimal cost to reach the end, once found.
+	minimalEndCost := -1
+
+	for pq.Len() > 0 {
+		current := heap.Pop(pq).(*State)
+
+		// If we have found an end cost, and this state's cost is greater than that minimal cost, we can stop.
+		// (Because the priority queue always gives us states in ascending order of cost, no cheaper or equal path
+		// to the end can appear after we exceed the minimalEndCost.)
+		if minimalEndCost != -1 && current.cost > minimalEndCost {
+			break
+		}
+
+		// Check if we've reached the end node
+		if current.node.point == end {
+			// If this is the first time we reach the end, record this cost as minimal
+			if minimalEndCost == -1 {
+				minimalEndCost = current.cost
+			}
+			// Don't return immediately; continue to process other possible equal-cost paths
+			continue
+		}
+
+		// If this state is not better than a previously found cost, skip
+		if visited[current.node.point] != nil {
+			if bestCost, ok := visited[current.node.point][current.direction]; ok && bestCost < current.cost {
+				continue
+			}
+		}
+
+		// Process neighbors
+		for _, edge := range current.node.edges {
+			turnCost := 0
+			if edge.direction != current.direction {
+				turnCost = 1000
+			}
+			newCost := current.cost + turnCost + edge.cost
+
+			// If we already have an end cost and this new cost is worse, skip
+			if minimalEndCost != -1 && newCost > minimalEndCost {
+				continue
+			}
+
+			// Check if this new path to edge.to.node & edge.direction is better or equal to previously known paths
+			if visited[edge.to.point] == nil {
+				visited[edge.to.point] = make(map[int]int)
+			}
+
+			prevCost, found := visited[edge.to.point][edge.direction]
+			if !found || newCost < prevCost {
+				// Found a strictly better path
+				visited[edge.to.point][edge.direction] = newCost
+
+				if parents[edge.to.point] == nil {
+					parents[edge.to.point] = make(map[int][]struct {
+						node      ReindeerMazePoint
+						direction int
+					})
+				}
+				// Reset parents for this state because we found a strictly better path
+				parents[edge.to.point][edge.direction] = []struct {
+					node      ReindeerMazePoint
+					direction int
+				}{
+					{current.node.point, current.direction},
+				}
+
+				heap.Push(pq, &State{
+					node:      edge.to,
+					direction: edge.direction,
+					cost:      newCost,
+				})
+			} else if newCost == prevCost {
+				// Found another minimal path of the same cost
+				parents[edge.to.point][edge.direction] = append(
+					parents[edge.to.point][edge.direction],
+					struct {
+						node      ReindeerMazePoint
+						direction int
+					}{current.node.point, current.direction},
+				)
+				// No need to push to pq because this cost is already known
+			}
+		}
+	}
+
+	// If we never found the end, return -1 and no paths
+	if minimalEndCost == -1 {
+		return -1, nil
+	}
+
+	// Reconstruct all minimal-cost paths
+	allPaths := reconstructAllPaths(parents, visited, end, minimalEndCost)
+
+	return minimalEndCost, allPaths
+}
+
+// reconstructAllPaths reconstructs all of the minimal paths from the parents map
+func reconstructAllPaths(
+	parents map[ReindeerMazePoint]map[int][]struct {
+		node      ReindeerMazePoint
+		direction int
+	},
+	visited map[ReindeerMazePoint]map[int]int,
+	end ReindeerMazePoint,
+	minCost int,
+) [][]ReindeerMazePoint {
+	allPaths := [][]ReindeerMazePoint{}
+	// The end node could be reached with multiple directions
+	for dir, c := range visited[end] {
+		if c == minCost {
+			// Reconstruct all paths that end at (end, dir)
+			paths := reconstructPaths(parents, end, dir)
+			allPaths = append(allPaths, paths...)
+		}
+	}
+	return allPaths
+}
+
+// reconstructPaths finds the paths recursively from the parents map
+func reconstructPaths(
+	parents map[ReindeerMazePoint]map[int][]struct {
+		node      ReindeerMazePoint
+		direction int
+	},
+	current ReindeerMazePoint,
+	currentDirection int,
+) [][]ReindeerMazePoint {
+	// If no parents, this might be the start node
+	if parents[current] == nil || len(parents[current][currentDirection]) == 0 {
+		// We assume the current node is the start node
+		// Adjust this if you explicitly know the start
+		return [][]ReindeerMazePoint{{current}}
+	}
+
+	var allPaths [][]ReindeerMazePoint
+	for _, p := range parents[current][currentDirection] {
+		subPaths := reconstructPaths(parents, p.node, p.direction)
+		for _, sp := range subPaths {
+			allPaths = append(allPaths, append(sp, current))
+		}
+	}
+	return allPaths
+}
+
+// expandAllPaths takes the provided graph nodes and finds every maze point that
+// the path touches
+func expandAllPaths(allPaths [][]ReindeerMazePoint, graph ReindeerMazeGraph) [][]ReindeerMazePoint {
+	expandedPaths := make([][]ReindeerMazePoint, 0, len(allPaths))
+
+	for _, path := range allPaths {
+		// Each path is a list of graph nodes (decision points)
+		if len(path) == 0 {
+			continue
+		}
+		expandedPath := []ReindeerMazePoint{path[0]} // start from the first node
+
+		for i := 0; i < len(path)-1; i++ {
+			startNode := path[i]
+			endNode := path[i+1]
+
+			// Find the edge connecting startNode -> endNode
+			edge := findEdge(graph, startNode, endNode)
+			if edge == nil {
+				// This should not happen if the graph is consistent
+				continue
+			}
+
+			// We have startNode and know edge.direction and edge.cost
+			// Let's expand intermediate cells
+			curX, curY := startNode.X, startNode.Y
+			dx := directionDeltas[edge.direction].dx
+			dy := directionDeltas[edge.direction].dy
+
+			for step := 0; step < edge.cost; step++ {
+				curX += dx
+				curY += dy
+				expandedPath = append(expandedPath, ReindeerMazePoint{X: curX, Y: curY})
+			}
+		}
+
+		expandedPaths = append(expandedPaths, expandedPath)
+	}
+
+	return expandedPaths
+}
+
+// findEdge returns the edge that was traveled
+func findEdge(graph ReindeerMazeGraph, from, to ReindeerMazePoint) *ReindeerMazeEdge {
+	node := graph[from]
+	if node == nil {
+		return nil
+	}
+	for _, e := range node.edges {
+		if e.to.point == to {
+			return &e
+		}
+	}
+	return nil
 }
--- exercise/day16_test.go
+++ exercise/day16_test.go
@@ -88,4 +88,65 @@ func TestDay16Part1Ex2(t *testing.T) {
 }
 
 func TestDay16Part2(t *testing.T) {
+	input := []string{
+		"###############",
+		"#.......#....E#",
+		"#.#.###.#.###.#",
+		"#.....#.#...#.#",
+		"#.###.#####.#.#",
+		"#.#.#.......#.#",
+		"#.#.#####.###.#",
+		"#...........#.#",
+		"###.#.#####.#.#",
+		"#...#.....#.#.#",
+		"#.#.#.###.#.#.#",
+		"#.....#...#.#.#",
+		"#.###.#.#.#.#.#",
+		"#S..#.....#...#",
+		"###############",
+	}
+
+	d16 := Day16{}
+
+	maze := d16.parseInput(input)
+
+	countVisitedNodes := d16.Part2(maze)
+	expectedcountVisitedNodes := 45
+
+	if countVisitedNodes != expectedcountVisitedNodes {
+		t.Errorf("Day 16 - Part 2 (visited nodes) Test:\nwant %v\ngot %v\n", expectedcountVisitedNodes, countVisitedNodes)
+	}
+}
+
+func TestDay16Part2Ex2(t *testing.T) {
+	input := []string{
+		"#################",
+		"#...#...#...#..E#",
+		"#.#.#.#.#.#.#.#.#",
+		"#.#.#.#...#...#.#",
+		"#.#.#.#.###.#.#.#",
+		"#...#.#.#.....#.#",
+		"#.#.#.#.#.#####.#",
+		"#.#...#.#.#.....#",
+		"#.#.#####.#.###.#",
+		"#.#.#.......#...#",
+		"#.#.###.#####.###",
+		"#.#.#...#.....#.#",
+		"#.#.#.#####.###.#",
+		"#.#.#.........#.#",
+		"#.#.#.#########.#",
+		"#S#.............#",
+		"#################",
+	}
+
+	d16 := Day16{}
+
+	maze := d16.parseInput(input)
+
+	countVisitedNodes := d16.Part2(maze)
+	expectedcountVisitedNodes := 64
+
+	if countVisitedNodes != expectedcountVisitedNodes {
+		t.Errorf("Day 16 - Part 2 (visited nodes) Test:\nwant %v\ngot %v\n", expectedcountVisitedNodes, countVisitedNodes)
+	}
 }
